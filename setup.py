ac72fe0b9805d71ace3dc97930575f82d7c36163
Sergio Infante <raulsergio9@gmail.com>
2021-05-23 11:40:57 -0500

    Update pypi classifiers about development status, environment, intended audience, and topics

--- setup.py
+++ setup.py
@@ -6,20 +6,26 @@ with open("README.md", 'r') as fh:
 
 setup(
     name='random-address',
-    version='0.1.0',
+    version='0.1.1',
     description='Tool to retrieve a real random address '
                 'that geocode successfully',
     packages=['random_address'],
     package_data={'random_address': ['addresses-us-all.json']},
     include_package_data=True,
     classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities"
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
